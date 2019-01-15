04c2bb284204e9195526d39e7068a5c0b6c8b51f
Jaosn Westbrook <westbrook.jason@gmail.com>
2019-01-15 15:07:06 -0500

    new file:   .gitignore 	modified:   bin/Lab3.class 	modified:   src/Lab3.java

--- src/Lab3.java
+++ src/Lab3.java
@@ -8,9 +8,11 @@ public class Lab3 {
 		Scanner scan = new Scanner (System.in);
 		int iNum = 0;
 		String name = "";
-		String cont = "n";
+		String str1 = "";
+		char cont = 'n';
 		
 		do {
+		
 		System.out.println("Welcome to the Integral Integer Fun Machine!");
 		
 		System.out.print("Please enter your name: ");
@@ -39,17 +41,21 @@ public class Lab3 {
 			System.out.println(name + ", your number is Odd and over 60");
 		}
 		
+		do {
 		System.out.println("Would you like to play again " + name + "? (y/n)");
-		cont = scan.nextLine();
+		//cont = scan.nextLine();
+		cont = scan.next().charAt(0);
+		str1 = scan.nextLine(); //this is done to consume the new line from scan.next()
+		if (cont != 'n' && cont != 'N' && cont != 'y' && cont != 'Y') {
+			System.out.println("Invalid input! Please try again. ");
+		}
+		
+		}while (cont != 'n' && cont != 'N' && cont != 'y' && cont != 'Y');
 		
 		
+		}while (cont != 'n' && cont != 'N');
 		
-		
-		
-		
-		}while (cont != "n" && cont !="N");
-		
-		System.out.println("Thank you come again!");
+		System.out.println("Goodbye, " + name + "!!!");
 		scan.close();
 	}
 
